48ad0901d5caf4f756802a92fd8d0cb7e85c02e4
Mia Seo <tjwlfif@gmail.com>
2023-03-06 09:58:17 +0900

    Add: 파일 기초 세팅 완료 - Main : 기본 메인 화면 - MainRes : sidebar가 열렸을 때 보여줄 반응형 화면 - Nav : 네비게이터 - SideBar : 메인에서 선택한 도시에 대한 내용을 보여주는 sidebar

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,21 @@
 import Head from 'next/head';
 import Image from 'next/image';
+import Main from './components/Main';
+import MainRes from './components/MainRes';
+import Nav from './components/Nav';
+import SideBar from './components/SideBar';
 import { Inter } from '@next/font/google';
 import styles from '@/styles/Home.module.css';
+import { useState } from 'react';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
-  return <div>안녕하세요</div>;
+  const [isSideBarOpen, setIsSideBarOpen] = useState(false);
+  return (
+    <div>
+      <Nav />
+      {isSideBarOpen ? <MainRes /> : <Main />}
+    </div>
+  );
 }
